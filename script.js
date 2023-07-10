6014e473346a137aba93e392eb8dbed619f2ea20
Eesha Singh <121451707+eesha2501@users.noreply.github.com>
2023-07-10 16:47:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function scrollToServices() {
+  // Find the services section by its ID
+  const servicesSection = document.getElementById('services');
+
+  // Scroll smoothly to the services section
+  servicesSection.scrollIntoView({ behavior: 'smooth' });
+}
